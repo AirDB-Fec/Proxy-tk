f06b3e171367d4dc131ebb33644ab477bec2d192
Tony <tony.kijak@gmail.com>
2018-08-24 16:25:56 -0700

    passes data into script template for hydration

--- templates/scripts.js
+++ templates/scripts.js
@@ -1,9 +1,9 @@
-module.exports = () => `
+module.exports = photos => `
   <script crossorigin src="https://unpkg.com/react@16/umd/react.development.js"></script>
   <script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script>
   <script type="text/javascript" src="bundle.js"></script>
   <script>
-    ReactDOM.hydrate(React.createElement(Applet), document.getElementById('photo'));
+    ReactDOM.hydrate(React.createElement(Applet, { photos: ${JSON.stringify(photos)} }), document.getElementById('photo'));
     ReactDOM.hydrate(React.createElement(Footer), document.getElementById('footer'));
   </script>
 `;
